55a42033426591defabc88b40662b14b9f61edc0
niikari <pakniiles@gmail.com>
2021-10-27 13:28:41 +0300

    initial commit

---
+++ src/test/java/com/ohjelmistoprojekti/kysely/KyselyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohjelmistoprojekti.kysely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyselyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
