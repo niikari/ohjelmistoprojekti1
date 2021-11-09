9c2a69154c99086036dd86d3df3ba08869da721b
niikari <pakniiles@gmail.com>
2021-11-09 15:49:43 +0200

    uusi branchi

--- src/main/java/com/ohjelmistoprojekti/kysely/controllers/KyselyRestControllers.java
+++ src/main/java/com/ohjelmistoprojekti/kysely/controllers/KyselyRestControllers.java
@@ -1,10 +1,23 @@
 package com.ohjelmistoprojekti.kysely.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ohjelmistoprojekti.kysely.models.MakerAnswer;
+import com.ohjelmistoprojekti.kysely.repos.MakerAnswerRepository;
 
 @RestController
 public class KyselyRestControllers {
 
+	@Autowired
+	private MakerAnswerRepository makerAnswerRepository;
 	
+	@GetMapping("/")
+	public MakerAnswer report() {
+		
+		
+		return null;
+	}
 	
 }
