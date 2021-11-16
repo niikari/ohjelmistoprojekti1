02f36938a28aae83e6d08370aa98670cdd45f72b
niikari <pakniiles@gmail.com>
2021-11-16 14:44:45 +0200

    endpoint /reports/{inquiry_id} lisätty

--- src/main/java/com/ohjelmistoprojekti/kysely/controllers/KyselyRestControllers.java
+++ src/main/java/com/ohjelmistoprojekti/kysely/controllers/KyselyRestControllers.java
@@ -1,21 +1,19 @@
 package com.ohjelmistoprojekti.kysely.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ohjelmistoprojekti.kysely.models.MakerAnswer;
-import com.ohjelmistoprojekti.kysely.repos.InquiryRepository;
 import com.ohjelmistoprojekti.kysely.repos.MakerAnswerRepository;
 
 
 @RestController
 public class KyselyRestControllers {
-
-	@Autowired
-	private InquiryRepository inqRepository;
 	
 	@Autowired
 	private MakerAnswerRepository makerAnswerRepository;
@@ -27,6 +25,18 @@ public class KyselyRestControllers {
 		return (List<MakerAnswer>) makerAnswerRepository.findAll();
 	}
 	
-	
+	@GetMapping("/reports/{id}")
+	public List<MakerAnswer> getReportsByInquiry(@PathVariable Long id) {
+		List<MakerAnswer> modAnswers = new ArrayList<>();		
+		List<MakerAnswer> makeranswers = (List<MakerAnswer>) makerAnswerRepository.findAll();
+		
+		for (MakerAnswer ans : makeranswers) {
+			if (ans.getAnswer().getQuestion().getInquiry().getId() == id) {
+				modAnswers.add(ans);
+			}
+		}		
+		
+		return modAnswers;
+	}
 		
 }
