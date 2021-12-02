ad1a9ff6dfe8e66d6922ddb34f777703af6a5fd4
niikari <pakniiles@gmail.com>
2021-12-02 09:52:40 +0200

    makers avattu

--- src/main/java/com/ohjelmistoprojekti/kysely/configurations/SecurityConfig.java
+++ src/main/java/com/ohjelmistoprojekti/kysely/configurations/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
       .antMatchers(HttpMethod.GET, "/makeranswers", "/makeranswers/**").permitAll()
       .antMatchers(HttpMethod.POST, "/api/makerAnswers").permitAll()
       .antMatchers(HttpMethod.POST, "/api/makers").permitAll()
-      .antMatchers(HttpMethod.GET, "/api/makers").permitAll()
+      .antMatchers(HttpMethod.GET, "/api/makers", "/api/makers/**").permitAll()
           .anyRequest().authenticated()
           .and()
           // Filter for the api/login requests
